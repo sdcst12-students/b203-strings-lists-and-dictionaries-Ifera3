6054fe5df97dd7914b05a901487371ef8d4c35ef
Ifera3 <talant994@deltalearns.ca>
2024-11-15 15:33:27 -0800

    done 11/15

--- task5.py
+++ task5.py
@@ -7,8 +7,9 @@ def main():
         action = "\'" + input("get iteam or drop iteam: ")
         if '\'end' in action:
             break
-        elif '\'get' in action:
-            action = action.replace("\'get ", "")
+        elif '\'get' in action or '\'g' in action:
+            action = action.replace("\'get ", " ")
+            action = action.replace("\'g ", " ")
             action = action.split(" ")
             iteam = ''
             a = 1
@@ -23,8 +24,9 @@ def main():
                 invatory[iteam] = invatory[iteam] + a
             else:
                 invatory[iteam] = a
-        elif '\'drop' in action:
-            action = action.replace("\'drop ", "")
+        elif '\'drop' in action or '\'d' in action:
+            action = action.replace("\'drop ", " ")
+            action = action.replace("\'d ", " ")
             action = action.split(" ")
             iteam = ''
             a = 1
@@ -44,11 +46,21 @@ def main():
                     invatory[iteam] = invatory[iteam] - a
             else:
                 print(f"{iteam} not in your invatory")            
-        elif '\'show invatory' in action:
+        elif '\'show invatory' in action or '\'si' in action:
             print(invatory)
+            tableLingth = 5
             for i in invatory:
-                tableLingth = 7 + len(i) + len(str(invatory[i]))
+                if tableLingth < 7 + len(i) + len(str(invatory[i])):
+                    tableLingth = 7 + len(i) + len(str(invatory[i]))
                 print(tableLingth)
+            print('-' * tableLingth)
+            for i in invatory:
+                if tableLingth > 7 + len(i) + len(str(invatory[i])):
+                    a = ' ' * (tableLingth - (7 + len(i) + len(str(invatory[i]))))
+                    print(f'| {i} | {invatory[i]} {a}|')
+                else:
+                    print(f'| {i} | {invatory[i]} |')
+                print('-' * tableLingth)
         else:
             print("Not valid acction")
             if input("End code? ") == 'yes':
--- zChallenge.py
+++ zChallenge.py
@@ -1,18 +1,14 @@
 #!python3
 # Explain what this code does
 
-import random
+import random #inports functions from a libary called random that adds frunctions based around randomly genrating numbers
 
-x = []
+x = [] #creats a empty list called x
 
-for i in range(40):
-    if random.randint(0,1):
-        x.append(random.randint(1,10))
+for i in range(40): #repeats code 40 times
+    if random.randint(0,1): #randomly choses 0 or 1 to use the if statment to randomly choose if it is true or false
+        x.append(random.randint(1,10)) # creats a random intager from 1 to 10 and adds it to the list x
     else:
-        x.append(random.randint(1,10) + random.randint(1,10)/10)
+        x.append(random.randint(1,10) + random.randint(1,10)/10) # creats a random decimal number with one decimale place from 1.1 to 11.0 and adds it to the list x
 
-print(x)
-
-"""
-
-"""
+print(x) #prints the list called x with the 40 numbers in it
